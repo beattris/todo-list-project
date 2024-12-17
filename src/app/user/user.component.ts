b1aedc9c503a749b429f37f4830acca9e2016f21
beattris <beatriceebirim@gmail.com>
2024-12-17 05:17:42 +0100

    listened to the btns click event and changed the displayed user on click

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-
 // logic to generate a random index that is within the DUMMY_USERS array.
 const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -17,5 +16,10 @@ export class UserComponent {
   // Used a getter to concatenate the user images folder path to match the users data images
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randonIndex];
   }
 }
