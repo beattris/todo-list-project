2dc6a498ad7eca3172de081b29bf4436b833c049
beattris <beatriceebirim@gmail.com>
2024-12-17 05:06:56 +0100

    used a getter fot the computed user image

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,8 +9,13 @@ const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randonIndex];
+
+  // Used a getter to concatenate the user images folder path to match the users data images
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
