9ad48c76d49a5a2ffa81c7a3eda89a3d8c0e7087
beattris <beatriceebirim@gmail.com>
2024-12-17 04:50:40 +0100

    dynamically outputted a random user from the DUMMY_USERS array

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+
+// logic to generate a random index that is within the DUMMY_USERS array.
+const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randonIndex];
 }
