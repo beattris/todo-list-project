c9c924b891a2a995f349f667ecaa4acd02be3fcc
beattris <beatriceebirim@gmail.com>
2024-12-23 03:54:51 +0100

    used to signal and computed fxns to get and manage the users and their state

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 // logic to generate a random index that is within the DUMMY_USERS array.
@@ -11,15 +11,18 @@ const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randonIndex];
+  selectedUser = signal(DUMMY_USERS[randonIndex]);
+  // how to get the imagePath, now that a signal is used for the state
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   // Used a getter to concatenate the user images folder path to match the users data images
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
     const randonIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randonIndex];
+    // setting the signal
+    this.selectedUser.set(DUMMY_USERS[randonIndex]);
   }
 }
