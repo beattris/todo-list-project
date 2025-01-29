819717a2e88e8fc642a8dea7b06a7de770126fda
beattris <beatriceebirim@gmail.com>
2025-01-29 06:01:24 +0100

    handled the form configuration and wrote the form submission logic to add a new task

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancelTask = new EventEmitter();
+  @Output() addTask = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancelTask() {
-    this.cancelTask.emit()
+    this.cancelTask.emit();
+  }
+
+  onSubmit() {
+    this.addTask.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
   }
 }
